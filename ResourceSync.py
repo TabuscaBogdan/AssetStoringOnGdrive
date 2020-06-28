ce3aa0adacbe1f5ed37319f0915d271a9b23fa5f
Bogdan <bogdan.tabusca@gmail.com>
2020-06-28 14:35:25 +0300

    If ou want only new files to be downloaded.

--- ResourceSync.py
+++ ResourceSync.py
@@ -43,6 +43,8 @@ def downloadFiles(jsonData,baseFolder):
         if "." in key:
             if not os.path.isdir(baseFolder):
                 os.mkdir(baseFolder)
+            #Uncomment This and tab line 48 if you only want to download missing files
+            #if not os.path.exists(destination):
             download_file_from_google_drive(id=includedObject,destination=destination)
         else:
             downloadFiles(includedObject,destination)
